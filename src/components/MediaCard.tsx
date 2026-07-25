ac518f2d5aa2bbdba858f4d93d4ec02fe1d3b690
Quan Pham <quanlong.pham@gmail.com>
2026-07-24 17:17:49 -0700

    Fixed card overlay
    Fixed card content overlapping too much with image and clipping out text.

--- src/components/MediaCard.tsx
+++ src/components/MediaCard.tsx
@@ -14,17 +14,18 @@ interface MediaCardProps extends React.HTMLProps<HTMLDivElement> {
 
 export default function MediaCard(props: MediaCardProps) {
   return (
-    <Card className={props.className} sx={{ position: 'relative', color: 'unset' }}>
-      <CardMedia
+    <Card className={props.className} sx={{ backgroundColor: 'black', color: 'unset' }}>
+      <CardMedia sx={{ position: 'relative' }}
         image={props.image}
         title={props.altText}
-      />
-      <CardContent 
-        sx={{ 
-            position: 'absolute', height: '30%', bottom: 0, width: '100%',
-            background: 'linear-gradient(0deg, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 1) 80%, rgba(0, 0, 0, 0) 100%)'
-        }}>
-        <p style={{ color: 'white', fontWeight: 500, fontSize: '1.2rem' }}>
+      >
+        <div style={{
+          position: 'absolute', height: '10%', bottom: 0, width: '100%',
+          background: 'linear-gradient(0deg, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0) 100%)'
+        }}/>
+      </CardMedia>
+      <CardContent sx={{ padding: '0 1rem !important' }}>
+        <p style={{ color: 'white', fontWeight: 500, fontSize: '1.2rem', marginTop: '0.5rem' }}>
             {props.captionTitle}
         </p>
         <p style={{ lineHeight: '2rem' }}>

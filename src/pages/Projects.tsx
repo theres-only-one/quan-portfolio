477b20eb2b125a55ac107164f8e1af5b58368d0c
Quan Pham <quanlong.pham@gmail.com>
2026-08-28 17:18:47 -0700

    Modified carousel and background
    Modified carousel sizing and added background fade in projects page.

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -5,14 +5,36 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 export default function Projects() {
   return (
     <main>
-      <div className="home-page">
+      <div className="home-page"> 
         <h1 className="section-title garamond">My Projects</h1>
-        
         <img src="projectpage2.gif" className="background"/>
+        <div className="background-gradient"/>
       </div>
 
       <div className="home-page second-page">
-
+        <h1 className='garamond' style={{margin: "2.5rem 0", fontSize: "7rem", fontWeight: "200"}}>Overview</h1>
+        <Swiper className="page2Carousel"
+          modules={[Autoplay, Pagination, Navigation]}
+          autoplay={{ delay: 5000 }}  
+          pagination={{ clickable: true }}
+          loop
+          navigation
+          spaceBetween={0}
+          slidesPerView={3}
+        >
+          {
+            [
+              "about1.jpg",
+              "about2.JPG",
+              "about3.JPG",
+              "about4.jpg",
+              "about5.jpg",
+              "about6.jpg",
+            ].map((img) =>
+              <SwiperSlide key={img}><img src={img}/></SwiperSlide>
+            )
+          }
+        </Swiper>
       </div>
     </main>
   );
